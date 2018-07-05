7100e6749664141951b0a00e580122acb40cdebc
sriram sundharesan <sriram.sundhar@gmail.com>
2018-07-04 23:54:25 -0400

    Fix tests

--- src/test/java/stream/TransCodeTest.java
+++ src/test/java/stream/TransCodeTest.java
@@ -23,17 +23,17 @@ public class TransCodeTest {
 
     @Test
     public void horizontalTransform() {
-        assertEquals("Transformed string is", ";lk", transCode.horizontalTransCode(InputText.getInstance("asd")).getInputString());
-        assertEquals("Transformed string is", ",mn", transCode.horizontalTransCode(InputText.getInstance("cvb")).getInputString());
-        assertEquals("Transformed string is", "654", transCode.horizontalTransCode(InputText.getInstance("567")).getInputString());
-        assertEquals("Transformed string is", " |!", transCode.horizontalTransCode(InputText.getInstance(" |!")).getInputString());
+        assertEquals("Transformed string is", ";lk", transCode.horizontalTransCode(InputText.getInstance("asd", 1)).getInputString());
+        assertEquals("Transformed string is", ",mn", transCode.horizontalTransCode(InputText.getInstance("cvb", 1)).getInputString());
+        assertEquals("Transformed string is", "654", transCode.horizontalTransCode(InputText.getInstance("567", 1)).getInputString());
+        assertEquals("Transformed string is", " |!", transCode.horizontalTransCode(InputText.getInstance(" |!", 1)).getInputString());
     }
 
     @Test
     public void verticalTranscode() {
-        assertEquals("Transformed string is", ",mn", transCode.verticalTranscode(InputText.getInstance("876")).getInputString());
-        assertEquals("Transformed string is", "fgh", transCode.verticalTranscode(InputText.getInstance("RTY")).getInputString());
-        assertEquals("Transformed string is", " `\\", transCode.verticalTranscode(InputText.getInstance(" `\\")).getInputString());
+        assertEquals("Transformed string is", ",mn", transCode.verticalTranscode(InputText.getInstance("876", 1)).getInputString());
+        assertEquals("Transformed string is", "fgh", transCode.verticalTranscode(InputText.getInstance("RTY", 1)).getInputString());
+        assertEquals("Transformed string is", " `\\", transCode.verticalTranscode(InputText.getInstance(" `\\", 1)).getInputString());
 
     }
 
